3cf29f66b03c31a45fee5892713286eb53b9b55e
Bhaskar Reddy <164631537+Bhas90@users.noreply.github.com>
2025-11-30 17:46:41 +0530

    Update BannerForm.jsx

--- frontend/src/BannerForm.jsx
+++ frontend/src/BannerForm.jsx
@@ -74,7 +74,7 @@ const BannerForm = () => {
 
     try {
       await axios.post(
-        "http://localhost:5000/home/send-email",
+        "https://api.sujay-sierra.com/home/send-email",
         {
           name: formData.name,
           email: formData.email,
